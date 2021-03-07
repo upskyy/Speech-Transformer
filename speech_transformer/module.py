98217a74a6f596952030283738bf014d33468bfb
Sangchun Ha <seomk9896@naver.com>
2021-03-07 17:49:03 +0900

    Update module.py

--- speech_transformer/module.py
+++ speech_transformer/module.py
@@ -21,13 +21,13 @@ import torch.nn as nn
 class PositionalEncoding(nn.Module):
     def __init__(
             self,
-            d_model: int = 512,
+            model_dim: int = 512,
             max_len: int = 5000
     ) -> None:
         super(PositionalEncoding, self).__init__()
-        pe = torch.zeros(max_len, d_model, requires_grad=False)
+        pe = torch.zeros(max_len, model_dim, requires_grad=False)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float) * -(math.log(10000.0) / d_model))
+        div_term = torch.exp(torch.arange(0, model_dim, 2, dtype=torch.float) * -(math.log(10000.0) / model_dim))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
         pe = pe.unsqueeze(0)
